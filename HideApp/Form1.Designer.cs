5def56182d967340dc8984b98b79760610878e22
wuzongwen <wuzongwen@live.com>
2019-02-26 18:18:52 +0800

    首次上传代码
    首次上传代码

---
+++ HideApp/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace HideApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.功能区 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.还原窗口ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.功能区.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // 功能区
+            // 
+            this.功能区.Controls.Add(this.button4);
+            this.功能区.Controls.Add(this.button1);
+            this.功能区.Controls.Add(this.button3);
+            this.功能区.Controls.Add(this.button2);
+            this.功能区.Location = new System.Drawing.Point(11, 137);
+            this.功能区.Name = "功能区";
+            this.功能区.Size = new System.Drawing.Size(276, 81);
+            this.功能区.TabIndex = 19;
+            this.功能区.TabStop = false;
+            this.功能区.Text = "功能区";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(146, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 28);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "窗口最大化";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "任务栏图标隐藏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(146, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 28);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "窗口最小化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "任务栏图标显示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 119);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "公告区";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Location = new System.Drawing.Point(7, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(263, 92);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HideApp.Properties.Resources.IMG_1195;
+            this.pictureBox1.Location = new System.Drawing.Point(298, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 207);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.还原窗口ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 48);
+            // 
+            // 还原窗口ToolStripMenuItem
+            // 
+            this.还原窗口ToolStripMenuItem.Name = "还原窗口ToolStripMenuItem";
+            this.还原窗口ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.还原窗口ToolStripMenuItem.Text = "还原窗口";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 224);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(437, 81);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "自定义窗口大小";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(338, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "应用";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(215, 48);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(215, 20);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(186, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "y轴:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(186, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x轴:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "高度:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "宽度:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 309);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.功能区);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "任务栏图标隐藏&窗口最小化";
+            this.功能区.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox 功能区;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 还原窗口ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HideApp/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace HideApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        public static extern int FindWindow(string 类名, string 程序标题);
+        [DllImport("user32.dll", EntryPoint = "ShowWindowAsync")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        private static extern bool CloseWindow(IntPtr hwn);
+        [DllImport("user32.dll", EntryPoint = "PostMessage")]
+        public static extern int PostMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SendMessage(IntPtr HWnd, uint Msg, int WParam, int LParam);
+        public const int WM_SYSCOMMAND = 0x112;
+        public const int SC_MINIMIZE = 0xF020;
+        public const int SC_MAXIMIZE = 0xF030;
+        public const uint WM_SYSCOMMAND2 = 0x0112;
+        public const uint SC_MAXIMIZE2 = 0xF030;
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+        /// <summary>
+        /// 设置目标窗体大小，位置
+        /// </summary>
+        /// <param name="hWnd">目标句柄</param>
+        /// <param name="x">目标窗体新位置X轴坐标</param>
+        /// <param name="y">目标窗体新位置Y轴坐标</param>
+        /// <param name="nWidth">目标窗体新宽度</param>
+        /// <param name="nHeight">目标窗体新高度</param>
+        /// <param name="BRePaint">是否刷新窗体</param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, bool BRePaint);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;                             //最左坐标
+            public int Top;                             //最上坐标
+            public int Right;                           //最右坐标
+            public int Bottom;                        //最下坐标
+        }
+
+        private const int SW_HIDE = 0;                                                          //常量，隐藏
+        private const int SW_SHOWNORMAL = 1;                                                    //常量，显示，标准状态
+        private const int SW_SHOWMINIMIZED = 2;                                                 //常量，显示，最小化
+        private const int SW_SHOWMAXIMIZED = 3;                                                 //常量，显示，最大化
+        private const int SW_SHOWNOACTIVATE = 4;                                                //常量，显示，不激活
+        private const int SW_RESTORE = 9;                                                       //常量，显示，回复原状
+        private const int SW_SHOWDEFAULT = 10;
+
+        //句柄
+        private int hwnd = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "安图恩：周4 5 6 晚6.50集合 -9点之前 肯定结束10波结束，有时候可能多开到11 12 13波" +
+"卢克修改为周日与周一，周日下午有周一下午可能也有晚上一定有团，全天大概20个团。";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToChange(IntPtr p, bool isboolean)
+        {
+            try
+            {
+                ITaskbarList bar = (ITaskbarList)new CoTaskbarList();
+                bar.HrInit();
+                if (!isboolean)
+                {
+                    //result = ShowWindowAsync(p, SW_SHOWNORMAL);
+                    bar.DeleteTab(p);
+                }
+                else
+                {
+                    //result = ShowWindowAsync(p, SW_HIDE);
+                    bar.AddTab(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #region Event
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hwnd = FindWindow(null, "不常用群聊"); //句柄是int32型的整数
+                //Hiden
+                if (hwnd != 0)
+                    ToChange(new IntPtr(hwnd), false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hwnd = FindWindow(null, "不常用群聊"); //句柄是int32型的整数
+                //Show
+                if (hwnd != 0)
+                    ToChange(new IntPtr(hwnd), true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        
+
+        //private void Form1_SizeChanged(object sender, EventArgs e)
+        //{
+        //    if (this.WindowState == FormWindowState.Minimized) //判断是否最小化
+        //    {
+        //        this.ShowInTaskbar = false; //不显示在系统任务栏
+        //        notifyIcon1.Visible = true; //托盘图标可见
+        //    }
+        //}
+
+        //private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        //{
+        //    this.ShowInTaskbar = true; //显示在系统任务栏
+        //    this.WindowState = FormWindowState.Normal; //还原窗体
+        //    notifyIcon1.Visible = false; //托盘图标隐藏
+        //}
+
+
+        void exitMenu_Click(object sender, EventArgs e)
+        {
+            this.Dispose(true);
+
+            Application.ExitThread();
+        }
+
+        #endregion
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MiniMizeAppication("TIM");
+        }
+
+        /// <summary>  
+        /// 最小化其他应用程序  
+        /// </summary>  
+        /// <param name="processName"></param>  
+        private void MiniMizeAppication(string processName)
+        {
+            Process[] processs = Process.GetProcessesByName(processName);
+            if (processs != null)
+            {
+                foreach (Process p in processs)
+                {
+                    IntPtr handle = new IntPtr(FindWindow(null, p.MainWindowTitle));
+                    PostMessage(handle, WM_SYSCOMMAND, SC_MINIMIZE, 0);
+                    MoveWindow(handle, 0, 0, 200, 150, false);
+                    RECT rect = new RECT();
+                    GetWindowRect(handle, ref rect);
+                    textBox2.Text = (rect.Right - rect.Left).ToString();                        //窗口的宽度
+                    textBox3.Text = (rect.Bottom - rect.Top).ToString();                   //窗口的高度
+                    textBox4.Text = rect.Left.ToString();
+                    textBox5.Text = rect.Top.ToString();
+                }
+            }
+        }
+
+        private void GetWindowSize() {
+
+        }
+
+        /// <summary>  
+        /// 最大化其他应用程序  
+        /// </summary>  
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("TIM");
+            if (processes.Length > 0)
+            {
+                IntPtr handle = processes[0].MainWindowHandle;
+                SendMessage(handle, WM_SYSCOMMAND2, new IntPtr(SC_MAXIMIZE2), IntPtr.Zero); // 最大化  
+                SwitchToThisWindow(handle, true);   // 激活  
+            }
+        }
+
+        //  只有Form_Closing事件中 e.Cancel可以用。
+        //  你的是Form_Closed事件。 Form_Closed事件时窗口已关了 ，Cancel没用了；
+        //  Form_Closing是窗口即将关闭时询问你是不是真的关闭才有Cancel事件
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            // 注意判断关闭事件reason来源于窗体按钮，否则用菜单退出时无法退出!
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                //取消"关闭窗口"事件
+                e.Cancel = true; // 取消关闭窗体 
+
+                //使关闭时窗口向右下角缩小的效果
+                this.WindowState = FormWindowState.Minimized;
+                this.notifyIcon1.Visible = true;
+                //this.m_cartoonForm.CartoonClose();
+                this.Hide();
+                return;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
